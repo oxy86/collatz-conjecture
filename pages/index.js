f277bbd6a1e8fe078353a7bd0151fadd844f0d65
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2022-10-06 15:06:31 +0300

    added GH code URL and icon :P

--- pages/index.js
+++ pages/index.js
@@ -320,13 +320,25 @@ export default function Home() {
 
       <footer className="min-h-[200px] flex w-full items-start justify-center p-5 border-t">
         <a
-          className="flex items-center justify-center gap-2"
+          className="flex items-center justify-center gap-2 px-2" 
           href="https://www.webzein.gr?utm_source=code&utm_medium=default&utm_campaign=collatz-conjecture"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Created with ❤️ by{' '}
+          Created with ❤️  by{' '}
           <Image src="/webzein.svg" alt="Webzein Logo" width={16} height={20} />
+        </a>
+        - 
+        <a
+          className="flex items-center justify-center gap-2 px-2"
+          href="https://github.com/oxy86/collatz-conjecture"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fork me at 
+          <svg height="16" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-mark-github v-align-middle">
+            <path fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+        </svg>
         </a>
       </footer>
     </div>
